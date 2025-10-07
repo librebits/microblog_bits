97c0b77e5681f33f26a5d2703a1b06668734c081
fenix <fenix@librebits.info>
2025-10-07 09:49:02 +0200

    ++ Jinja templating

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return"HOLA Mundo! :-)"
-
+    user = {'username': 'Fenix'}
+    return render_template ('index.html', title='Home', user=user)
